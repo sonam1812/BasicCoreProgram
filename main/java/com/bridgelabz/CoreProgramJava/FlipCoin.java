d5e410caa7621d37df973cd84db459df64a69242
sonam1812 <sonampal832@gmail.com>
2021-08-27 18:53:20 +0530

    Added Flip coin program

---
+++ main/java/com/bridgelabz/CoreProgramJava/FlipCoin.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.CoreProgramJava;
+import java.util.Scanner;
+
+public class FlipCoin {
+	
+	public static void main(String[] args) {
+		 String result;
+	        int countHead = 0;
+	        int countTail = 0;
+	        for (int i = 0; i <= 10; i++) {
+	            int flip = (int) Math.floor(Math.random() * 2);
+	            if (flip == 0) {
+	                result = "Heads";
+	                countHead++;
+	            } else result = "Tails";
+	            countTail++;
+	        }
+	        System.out.println(countHead);
+	        System.out.println(countTail);
+	    }
+	}
+
+	
+
+
